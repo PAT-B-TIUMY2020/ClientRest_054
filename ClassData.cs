f42a949ca0c730933292700a9d9a363b3c3fc3b0
Muhammad Ferdy <ferdyzecter@gmail.com>
2020-12-20 21:41:39 +0700

    Add project files.

---
+++ ClassData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Net;
+using static ClientRest_054_MuhammadFerdyYosa.Program;
+
+namespace ClientRest_054_MuhammadFerdyYosa
+{
+    class ClassData
+    {
+        public void getData()
+        {
+            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+
+            foreach (var mhs in data)
+            {
+                Console.WriteLine("NIM: " + mhs.nim);
+                Console.WriteLine("Nama: " + mhs.nama);
+                Console.WriteLine("NIM: " + mhs.prodi);
+                Console.WriteLine("NIM: " + mhs.angkatan);
+            }
+            Console.ReadLine();
+        }
+    }
+}
